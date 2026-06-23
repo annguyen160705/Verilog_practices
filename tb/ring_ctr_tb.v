6c36aa479626c7c0412ec0fe92fd08bb7dc5fcce
An Nguyen <annguyen160705@gmail.com>
2026-06-23 10:12:11 +0700

    ring_ctr

---
+++ tb/ring_ctr_tb.v
@@ -0,0 +1,19 @@
+module ring_ctr_tb;
+    parameter WIDTH = 4;
+    reg clk,rstn;
+    wire [WIDTH-1:0] out ;
+
+    ring_ctr #(WIDTH) u0 (clk,rstn,out);
+
+    always #10 clk = ~clk;
+
+    initial begin
+        {clk,rstn} = 0;
+
+        $monitor ("[%0t] rstn = %0h out=%0h",$time,rstn,out);
+        repeat(2) @(posedge clk);
+        rstn = 1;
+        repeat(15) @(posedge clk);
+        $finish;
+    end
+endmodule
