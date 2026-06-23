bae3b07260ed39e3744926d2428356cce4b7e812
An Nguyen <annguyen160705@gmail.com>
2026-06-23 10:08:40 +0700

    halfadder

---
+++ tb/halfadder_tb.v
@@ -0,0 +1,22 @@
+module halfadder_tb;
+
+    reg a,b;
+    wire sum,cout;
+    integer i;
+
+    halfadder u0 (.a(a), .b(b), .sum(sum), .cout(cout));
+
+    initial begin
+        a <= 0;
+        b <= 0;
+
+        $monitor ("a = %0b b = %0b sum = %0b cout = %0b", a, b, sum, cout);
+
+        for(i=0; i< 4; i = i +1) begin
+        {a, b} = i;
+        #10;
+        end
+    end
+
+
+endmodule
