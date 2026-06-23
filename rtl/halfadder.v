608183ba49ed9cb1e30579306c127ac275ebc3f6
An Nguyen <annguyen160705@gmail.com>
2026-06-23 20:15:31 +0700

    halfadder changes

--- rtl/halfadder.v
+++ rtl/halfadder.v
@@ -1,6 +1,7 @@
-module halfadder (input a,b,output sum,cout);
+module halfadder (input a, b, output reg sum,cout);
 
-    assign sum = a ^ b;
-    assign cout = a & b;
+    always @ (a or b) begin
+        {cout,sum} = a + b;
+    end
 
 endmodule
